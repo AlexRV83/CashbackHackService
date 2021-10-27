aa5f97fd26160eaa0aea9cc2d1c02ebcc250c866
AlexRV83 <leksrst@gmail.com>
2021-10-27 13:55:00 +0300

    CashBackHackServiceTest added

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,46 @@
+package ru.netology.service;
+
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+
+public class CashbackHackServiceTest {
+    public CashbackHackService CashbackHackService = new CashbackHackService();
+
+    @Test
+    void shouldCashBackSum900() {
+        int actual = CashbackHackService.remain(900);
+        int expected = 100;
+        assertEquals(actual,expected);
+
+    }
+    @Test
+    void shouldCashBackSum100() {
+        int actual = CashbackHackService.remain(100);
+        int expected = 900;
+        assertEquals(actual,expected);
+    }
+
+    @Test
+    void shouldCashBackSum0() {
+        int actual = CashbackHackService.remain(1000);// тест не проходит
+        int expected = 0;
+        assertEquals(actual,expected);
+    }
+    @Test
+    void shouldCashBackSum1100() {
+        int actual = CashbackHackService.remain(1100);
+        int expected = 900;
+        assertEquals(actual,expected);
+    }
+    @Test
+    void shouldCashBackSum5500() {
+        int actual = CashbackHackService.remain(5500);
+        int expected = 500;
+        assertEquals(actual,expected);
+    }
+
+
+}
